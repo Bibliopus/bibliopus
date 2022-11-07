6dad1ad27c4767f95cf78ca6bba18d343fb9f5d3
ColinEspinas <colin.espinas@yahoo.fr>
2022-11-07 22:26:29 +0100

    feat: renamed mobile app to shelf and added capacitor

---
+++ apps/mobile/shelf/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.bibliopus',
+  appName: '@bibliopus/shelf',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
