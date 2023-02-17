15ae076bbb8fc2723250d1450b863419cb858a39
ColinEspinas <colin.espinas@yahoo.fr>
2023-02-17 01:28:57 +0100

    fix: avoid 500 when a provider does not have book

--- apps/api/books/routes/editions/[isbn].get.ts
+++ apps/api/books/routes/editions/[isbn].get.ts
@@ -28,39 +28,56 @@ const getGoogleData = async (isbn: string) => {
 };
 
 const getOpenLibraryData = async (isbn: string) => {
-  const response = await fetch(`https://openlibrary.org/isbn/${isbn}.json`);
-  const data = await response.json();
+  try {
+    const response = await fetch(`https://openlibrary.org/isbn/${isbn}.json`);
+    const data = await response.json();
 
-  const workResponse = await fetch(`https://openlibrary.org${data.works[0].key}.json`);
-  const workData = await workResponse.json();
+    const workResponse = await fetch(`https://openlibrary.org${data.works[0].key}.json`);
+    const workData = await workResponse.json();
 
-  const subjects = workData.subjects?.map(category => ({ name: category })) ?? [];
+    const subjects = workData.subjects?.map(category => ({ name: category })) ?? [];
 
-  return {
-    subjects,
-    book: { olkey: workData.key, editions: [] },
-    cover: `https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`,
-  };
+    return {
+      subjects,
+      book: { olkey: workData.key, editions: [] },
+      cover: `https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`,
+    };
+  }
+  catch (err) {
+    return null;
+  }
 };
 
 export default defineEventHandler(async (event): Promise<Edition> => {
+  // Fix CORS Issues for now
+  event.node.res.setHeader('Access-Control-Allow-Origin', '*');
   try {
     const [googleData, openlibraryData] = await Promise.all([
       await getGoogleData(event.context.params.isbn),
       await getOpenLibraryData(event.context.params.isbn),
     ]);
 
-    // Fix CORS Issues for now
-    event.node.res.setHeader('Access-Control-Allow-Origin', '*');
+    let bookData = {} as Partial<Edition>;
 
-    return {
-      ...googleData,
-      ...openlibraryData,
-      subjects: [...googleData.subjects, ...openlibraryData.subjects],
-    } as Edition;
+    if (googleData) {
+      bookData = {
+        ...googleData,
+      };
+    }
+
+    if (openlibraryData) {
+      bookData = {
+        ...bookData,
+        ...openlibraryData,
+
+      };
+      if (googleData)
+        bookData.subjects = [...googleData.subjects, ...openlibraryData.subjects];
+    }
+
+    return bookData as Edition;
   }
   catch (err) {
-    // console.log(err);
     sendError(event, err);
   }
 });
