2c3fa060c9a2ec5e4fd301eaa94240ec7652b39d
Eliott Demont <eliott.demont@edu.devinci.fr>
2023-05-05 16:02:08 +0200

    feat: added collection item component

---
+++ packages/ui/components/atoms/collection/item.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { UseImage } from '@vueuse/components';
+
+const props = defineProps<{
+  user: string
+  name: string
+  covers: string[]
+}>();
+</script>
+
+<template>
+  <div class="p-3 w-fit bg-dune-900 border border-dune-800 rounded-md cursor-pointer hover:bg-dune-800 hover:border-dune-600 transition-colors">
+    <div class="max-h-[130px] overflow-hidden">
+      <div class="flex justify-center px-2 items-end">
+        <UseImage
+          :src="covers[0]"
+          class="relative rounded h-[150px] object-cover aspect-[180/270] -rotate-6 z-10"
+        />
+        <UseImage
+          :src="covers[1]"
+          class="relative rounded h-[150px] object-cover aspect-[180/270] -mx-10 z-20 mb-1"
+        />
+        <UseImage
+          :src="covers[2]"
+          class="relative rounded h-[150px] object-cover aspect-[180/270] rotate-6 z-30"
+        />
+      </div>
+    </div>
+    <div class="text-dune-50">
+      <p class="text-sm text-dune-300 mt-2 line-clamp-1">
+        {{ user }}
+      </p>
+      <h3 class="text-dune-50 line-clamp-1">
+        {{ name }}
+      </h3>
+    </div>
+  </div>
+</template>
