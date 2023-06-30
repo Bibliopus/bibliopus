ff6da077a485920eb89f537389e3a587a9a63fe9
ColinEspinas <colin.espinas@yahoo.fr>
2023-06-30 16:54:24 +0200

    fix: messages should now wrap correctly

--- packages/ui/components/atoms/message/item.vue
+++ packages/ui/components/atoms/message/item.vue
@@ -23,11 +23,11 @@ const formattedCreatedAt = computed(() => {
 
 <template>
   <li
-    class="flex flex-col gap-[2px]"
+    class="flex flex-col gap-[2px] w-full"
     :class="[sentByAuth ? 'self-end items-end' : 'self-start items-start']"
   >
     <p
-      class="border border-dune-800 p-3 text-dune-50 rounded-md w-max text-sm"
+      class="border border-dune-800 p-3 text-dune-50 rounded-md max-w-full text-sm break-words"
       :class="[sentByAuth ? 'rounded-br-sm bg-dune-900' : 'rounded-bl-sm bg-dune-950']"
     >
       {{ content }}
